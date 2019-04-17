32b15a1a05c97d33cd3fa51f5f92a4286d4e302d
cassfer <cassiohafernandes@gmail.com>
2019-04-17 03:04:26 -0300

    Versão 2.0 - Criada

---
+++ BKP NO INICIAR 2.0.bat
@@ -0,0 +1,42 @@
+@echo off
+
+color 17
+
+title BKP IN STARTUP
+
+psbklink.vbs "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\PSBK.lnk" "%cd%\PSBK.exe" /-y
+
+if exist "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\PSBK.lnk" (
+
+color 27
+
+    rem @echo off
+cls
+@echo.
+@echo    ******************************************************
+@echo    ***                                                ***
+@echo    ***   SISBKP INSERIDO NO INICIALIZAR COM SUCESSO   ***
+@echo    ***                                                ***
+@echo    ******************************************************
+@echo.
+) else (
+
+color 47
+
+    rem @echo off
+cls
+@echo.
+@echo    ******************************************************
+@echo    ***                                                ***
+@echo    ***       SISBKP NAO INSERIDO NO INICIALIZAR       ***
+@echo    ***                                                ***
+@echo    ******************************************************
+@echo.
+
+)
+
+explorer %AppData%\Microsoft\Windows\Start Menu\Programs\Startup
+
+pause
+
+@echo on
