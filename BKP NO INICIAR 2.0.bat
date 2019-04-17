a9db2c3b9211184704d30eaeebef8332ae80d463
cassfer <cassiohafernandes@gmail.com>
2019-04-17 03:25:19 -0300

    Versão 1.5 incluida, versão 2.0 revisada, foi removido as mensagens de 'deu certo' ou 'deu errado' do bat. Versão 1.5 é igual a antiga versão 2.0; Nova versão 2.0 é a final

--- BKP NO INICIAR 2.0.bat
+++ BKP NO INICIAR 2.0.bat
@@ -6,37 +6,6 @@ title BKP IN STARTUP
 
 psbklink.vbs "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\PSBK.lnk" "%cd%\PSBK.exe" /-y
 
-if exist "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\PSBK.lnk" (
-
-color 27
-
-    rem @echo off
-cls
-@echo.
-@echo    ******************************************************
-@echo    ***                                                ***
-@echo    ***   SISBKP INSERIDO NO INICIALIZAR COM SUCESSO   ***
-@echo    ***                                                ***
-@echo    ******************************************************
-@echo.
-) else (
-
-color 47
-
-    rem @echo off
-cls
-@echo.
-@echo    ******************************************************
-@echo    ***                                                ***
-@echo    ***       SISBKP NAO INSERIDO NO INICIALIZAR       ***
-@echo    ***                                                ***
-@echo    ******************************************************
-@echo.
-
-)
-
 explorer %AppData%\Microsoft\Windows\Start Menu\Programs\Startup
-
-pause
 
 @echo on
